25d227e565f8a62931270cdd3f213d8e0beaab68
kamna singh <71639968+Kamna-S@users.noreply.github.com>
2024-06-02 16:47:10 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,8 @@
+class pattern:
+    @staticmethod
+    def main():
+        for i in range(5,0,-1):
+            for j in range(1, i+1):
+                print(j,end = "")
+            print()
+pattern.main()
